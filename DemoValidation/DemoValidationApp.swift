6a39c2d752061c5f771f1c888120d67729a844ca
AvazbekNadyrbek <spatium.avazbek@gmail.com>
2025-10-21 17:34:08 +0600

    Initial Commit

---
+++ DemoValidation/DemoValidationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoValidationApp.swift
+//  DemoValidation
+//
+//  Created by Авазбек Надырбек уулу on 21.10.25.
+//
+
+import SwiftUI
+
+@main
+struct DemoValidationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
